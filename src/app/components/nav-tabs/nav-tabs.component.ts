8922294cafd6f0223e97947f3508ded240d16ab8
Gustavo <gustavo.camargos99@gmail.com>
2020-06-15 16:50:26 -0300

    Borrando fundo das tabs

--- src/app/components/nav-tabs/nav-tabs.component.ts
+++ src/app/components/nav-tabs/nav-tabs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'nav-tabs',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavTabsComponent implements OnInit {
 
-  constructor() { }
+  constructor
+  (
+    private router: Router
+  ) 
+  { 
+    router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd) {
+        let canvasWrapper = document.getElementById("canvas")
+        let routerContent = document.getElementById("container")
+        
+    
+        html2canvas(document.getElementById("container-blur")).then(canvas => {
+          canvasWrapper.appendChild(canvas)
+          canvasWrapper.style.transform = `translateY(-${window.innerHeight - 70}px)`
+        })
+
+        routerContent.onscroll = (scroll: any) => {
+          let scrolled = scroll.target.scrollTop
+
+          canvasWrapper.style.transform = `translateY(-${(window.innerHeight - 70) + scrolled}px)`
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
-    let router = document.getElementById("router-to-blur")
   }
 }
